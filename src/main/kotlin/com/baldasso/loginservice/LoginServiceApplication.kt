ba367545fea89853bcf6282198534a7d81615584
Jonatas Baldasso <jobaldasso@gmail.com>
2022-05-22 16:33:02 -0300

    Start project

---
+++ src/main/kotlin/com/baldasso/loginservice/LoginServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.baldasso.loginservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoginServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoginServiceApplication>(*args)
+}
